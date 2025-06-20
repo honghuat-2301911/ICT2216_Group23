5c43730248f6a453ac17af92068c6f00779dd8b8
Ong Hong Liang <2301859@sit.singaporetech.edu.sg>
2025-06-20 12:32:53 +0800

    removed some stuff

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def create_app():
                 template_folder="presentation/templates",
                 static_folder="presentation/static",
                 static_url_path="/static")
-    app.config['SECRET_KEY'] = 'd9a8f7c6e5b4d3c2b1a0f9e8d7c6b5a4'
+
 
     # register page-controller blueprints
     app.register_blueprint(login_bp)
