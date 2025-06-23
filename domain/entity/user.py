2615540e04bab43da8d989d3d9df8ed6f0227fe6
yan an <tyacya02@gmail.com>
2025-06-24 07:55:42 +0800

    edited entity setup and use flask g for entity caching to abide by getter and setter rules

--- domain/entity/user.py
+++ domain/entity/user.py
@@ -1,59 +1,26 @@
-from dataclasses import dataclass
-from typing import Optional
-
-
-@dataclass
 class User:
-    """
-    User entity that mirrors the `user` table in MySQL.
-    Compatible with `User(**row)` where row is a dict from SQL query.
-    """
-    id: int
-    name: str
-    password: str  # Hash in production
-    email: str
-    skill_lvl: Optional[str] = None
-    sports_exp: Optional[str] = None
-    role: str = "user"
+    def __init__(self, id, name, password, email, skill_lvl=None, sports_exp=None, role="user"):
+        self._id = id
+        self._name = name
+        self._password = password
+        self._email = email
+        self._skill_lvl = skill_lvl
+        self._sports_exp = sports_exp
+        self._role = role
 
+    # Getters
+    def get_id(self): return self._id
+    def get_name(self): return self._name
+    def get_password(self): return self._password
+    def get_email(self): return self._email
+    def get_skill_lvl(self): return self._skill_lvl
+    def get_sports_exp(self): return self._sports_exp
+    def get_role(self): return self._role
 
-    # ---------------- Getters ----------------
-    def get_id(self) -> int:
-        return self.id
-
-    def get_name(self) -> str:
-        return self.name
-
-    def get_password(self) -> str:
-        return self.password
-
-    def get_email(self) -> str:
-        return self.email
-
-    def get_skill_lvl(self) -> Optional[str]:
-        return self.skill_lvl
-
-    def get_sports_exp(self) -> Optional[str]:
-        return self.sports_exp
-
-    def get_role(self) -> str:
-        return self.role
-
-    # ---------------- Setters ----------------
-    def set_name(self, name: str) -> None:
-        self.name = name
-
-    def set_password(self, password: str) -> None:
-        self.password = password
-
-    def set_email(self, email: str) -> None:
-        self.email = email
-
-    def set_skill_lvl(self, skill_lvl: Optional[str]) -> None:
-        self.skill_lvl = skill_lvl
-
-    def set_sports_exp(self, sports_exp: Optional[str]) -> None:
-        self.sports_exp = sports_exp
-
-    def set_role(self, role: str) -> None:
-        self.role = role
+    # Setters
+    def set_name(self, name): self._name = name
+    def set_password(self, password): self._password = password
+    def set_email(self, email): self._email = email
+    def set_skill_lvl(self, skill_lvl): self._skill_lvl = skill_lvl
+    def set_sports_exp(self, sports_exp): self._sports_exp = sports_exp
+    def set_role(self, role): self._role = role
