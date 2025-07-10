c72da78943910853196a00db4b775d2786d62226
yan an <tyacya02@gmail.com>
2025-07-10 18:47:59 +0800

    trying unit test

---
+++ tests/unit/unit_test.py
@@ -0,0 +1,32 @@
+import unittest
+from app import app  # Assuming your Flask app is in app.py
+from flask import url_for
+from domain.control import bulletin_management
+
+class TestCreateActivity(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['TESTING'] = True
+        cls.client = app.test_client()
+
+
+    def test_host_activity_before_currentdate(self):
+        data = {
+            "activity_name": "Unit Test",
+            "activity_type": "Sports",
+            "skills_req": "Running",
+            "date": "2023-12-31T23:59",  # old date
+            "location": "Test Location",
+            "max_pax": 10
+        }
+        response = self.client.post(
+            "/host", 
+            data=data, 
+            follow_redirects=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Host form error: Date cannot be in the past", response.data)
+
+if __name__ == '__main__':
+    unittest.main()
