0597c69f346d56e16ec07e6b121864c6904a8f05
yan an <tyacya02@gmail.com>
2025-07-10 22:56:06 +0800

    manual creation of log files in test

--- tests/unit/unit_test.py
+++ tests/unit/unit_test.py
@@ -1,36 +1,12 @@
-import unittest
-import requests
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import pytest
+from app import create_app
 
-
-class TestCreateActivityLive(unittest.TestCase):
-
-    BASE_URL = "http://localhost:80"
-
-    def test_host_activity_before_currentdate(self):
-        data = {
-            "activity_name": "Unit Test",
-            "activity_type": "Sports",
-            "skills_req": "Running",
-            "date": "2023-12-31T23:59",  # old date
-            "location": "Test Location",
-            "max_pax": 10
-        }
-
-        # POST to the running Flask app in Docker
-        response = requests.post(
-            f"{self.BASE_URL}/host",
-            data=data,
-            verify=False  # don’t validate SSL
-        )
-
-        # Assertions
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("Date cannot be in the past", response.text)
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
 # import unittest
 # from app import create_app  # your factory function
